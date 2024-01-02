3b3a39bac044d8340bd0ed638183ee1d3de55888
Allan Yin <allanyin17@gmail.com>
2024-01-01 22:40:38 -0500

    remove error file

--- BinarySearch/Median2SortedArrays.cpp
+++ BinarySearch/Median2SortedArrays.cpp
@@ -1,45 +1,59 @@
 #include <vector>
 
 using namespace std;
-/*
-    - cannot simply join and do binary search, joining would be O(m) or O(n)
-    - the idea is to build the first half of the combined array  
-    - use binary search to look for this cuttof in the smaller sized array (doesn't really matter)
-*/
+
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // binary search nums1
-        int l = 0, r = nums1.size() - 1;
-        int total_size = nums1.size() + nums2.size();
-        int half_size = total_size/2;
-        double result = 0;
-
-        while (l <= r) {
-            int mid = l + (l-r)/2; // cutoff in nums1
-            int cor_mid = half_size - 2 - mid; // cutoff in nums2
-            int nums1_next = (mid + 1 < nums1.size()) ? nums1[mid+1] : INT_MAX;
-            int nums2_next = (cor_mid + 1 < nums2.size()) ? nums2[cor_mid + 1] : INT_MAX;
-
-            if ((nums1[mid] <= nums2_next) && (nums2[cor_mid] <= nums1_next)) {
-                // check condition, if valid, return median 
-                if (total_size % 2) {
-                    result = (double) min(nums1_next, nums2_next);
+        int m = nums1.size();
+        int n = nums2.size();
+        
+        if (m > n) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        
+        int total = m + n;
+        
+        int low = 0;
+        int high = m;
+        
+        double result = 0.0;
+        
+        while (low <= high) {
+            // nums1
+            int i = low + (high - low) / 2;
+            // nums2
+            int j = (total + 1) / 2 - i;
+            
+            int left1 = (i > 0) ? nums1[i - 1] : INT_MIN;
+            int right1 = (i < m) ? nums1[i] : INT_MAX;
+            int left2 = (j > 0) ? nums2[j - 1] : INT_MIN;
+            int right2 = (j < n) ? nums2[j] : INT_MAX;
+            
+            // partition is correct
+            if (left1 <= right2 && left2 <= right1) {
+                // even
+                if (total % 2 == 0) {
+                    result = (max(left1, left2) + min(right1, right2)) / 2.0;
+                // odd
                 } else {
-                    result = (max(nums1[mid], nums2[cor_mid]) + min(nums1_next, nums2_next)) / 2.0;
+                    result = max(left1, left2);
                 }
                 break;
+            } else if (left1 > right2) {
+                high = i - 1;
             } else {
-                // decide which direction to move mid for nums1 (opp for nums2)
-                if (nums1[mid] > nums2[nums2_next]) {
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
-                }
+                low = i + 1;
             }
         }
+        
         return result;
     }
 };
 
-int main () {}
+int main() {
+    Solution sol;
+    vector<int> nums1 = {1,3};
+    vector<int> nums2 = {2};
+    sol.findMedianSortedArrays(nums1, nums2);
+}
