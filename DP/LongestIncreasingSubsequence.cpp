ac55cc44158b432c0240f3e8eb1038106bc76915
Allan Yin <allanyin17@gmail.com>
2024-01-21 11:26:18 -0500

    21

---
+++ DP/LongestIncreasingSubsequence.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int maxLen = 0;
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLen = max(maxLen, dp[i]);
+        }
+        return maxLen;
+    }
+};
+
+int main() {}
