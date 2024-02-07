152f8f0fa909a62a231732dc56e4a97a71ce29e7
Allan Yin <a7yin@uwaterloo.ca>
2024-02-06 21:47:37 -0500

    daily leet

---
+++ DailyLeetcode/Feb6.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> freq;
+        int maxFreq = 0;
+
+        for (char &ch : s) {
+            freq[ch]++;
+            maxFreq = max(maxFreq, freq[ch]);
+        }
+
+        vector<vector<char>> buckets(maxFreq + 1);
+        // now, we add these to respective bucket 
+        for (auto &kv_pair : freq) {
+            buckets[kv_pair.second].push_back(kv_pair.first);
+        }
+
+        // now, build result
+        string result = "";
+        for (int i = buckets.size() - 1; i >= 0; i--) {
+            if (buckets[i].empty()) {
+                continue;
+            }
+
+            for (char ch : buckets[i]) {
+                result.append(i, ch);
+            }
+        }
+        return result;
+    }
+};
