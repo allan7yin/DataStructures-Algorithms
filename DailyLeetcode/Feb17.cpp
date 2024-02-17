2f68822aef00c9c7744219cf0888648f34bca2d4
Allan Yin <a7yin@uwaterloo.ca>
2024-02-17 10:20:15 -0500

    daily question

---
+++ DailyLeetcode/Feb17.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+/*
+    - We can solve this with DP -> slower than maxHeap approach 
+    - we can solve this with maxHeap
+*/
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        priority_queue<int> maxHeap;
+        int result = 0;
+        int n = heights.size();
+        for (int i = 1; i < n; i++) {
+            if (heights[i] > heights[i-1]) {
+                int diff = heights[i] - heights[i-1];
+                maxHeap.push(diff);
+                bricks -= diff;
+                if (bricks < 0) {
+                    ladders -= 1;
+                    bricks += maxHeap.top();
+                    maxHeap.pop();
+                }
+                if (ladders < 0) {
+                    result = i - 1;
+                    break;
+                }
+            }
+            result = i;
+        }
+        return result;
+    }
+};
