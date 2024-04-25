c0cb712ec0d0b8054826dbc444594bf87884cb6f
Allan Yin <a7yin@uwaterloo.ca>
2024-04-25 09:40:58 -0400

    Daily Question: Apeil 25th 2024

--- Cpp+Python/DailyLeetcode/April/April25.cpp
+++ Cpp+Python/DailyLeetcode/April/April25.cpp
@@ -7,11 +7,35 @@ using namespace std;
  * This is a DP question, we will maintain dp table, where table[i] represents
  * the longest ideal subsequence including s[i].
  *
- * the first solution is TLE -> brute-force. Now, look to reduce with memoization
+ * the first solution is TLE -> brute-force. Now, look to reduce with
+ * memoization
  */
 class Solution {
 public:
   int longestIdealString(string s, int k) {
+    vector<int> dp(26, 0);
+    for (int i = 0; i < s.size(); i++) {
+      int sub_max = 0;
+      char curr = s[i];
+      int index = curr - 'a';
+      int l = max(0, index - k);
+      int r = min(25, index + k);
+
+      for (int j = l; j <= r; j++) {
+        sub_max = max(sub_max, dp[j]);
+      }
+      dp[index] = sub_max + 1;
+    }
+
+    // go through the dp table and return the max length
+    int max_length = 0;
+    for (int i = 0; i < dp.size(); i++) {
+      max_length = max(max_length, dp[i]);
+    }
+    return max_length;
+  }
+
+  int longestIdealStringTLEVersion(string s, int k) {
     vector<string> dp(s.size(), "");
     dp[0] = s[0];
     for (int i = 1; i < s.size(); i++) {
