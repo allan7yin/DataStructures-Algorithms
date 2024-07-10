c10b2dca27896a2c6ef091dbb4f53e941b0d82ec
Allan Yin <a7yin@uwaterloo.ca>
2024-07-10 10:28:24 -0400

    small review

--- Main/DynamicProgramming/Review/Interval/1130.cc
+++ Main/DynamicProgramming/Review/Interval/1130.cc
@@ -1,6 +1,40 @@
 #include <vector>
 using namespace std;
 
+// let's try also doing this problem top-down
+int dp[40][40];
+int g[40][40];
+class Solution {
+  public:
+    int mctFromLeafValues(vector<int> &arr) {
+        int n = arr.size();
+        memset(dp, -1, sizeof(dp));
+        memset(g, 0, sizeof(g));
+        // first, precompute g
+
+        for (int i = n - 1; i >= 0; i--) {
+            g[i][i] = arr[i];
+            for (int j = i + 1; j < n; j++) {
+                g[i][j] = max(g[i][j - 1], arr[j]);
+            }
+        }
+        return f(0, n - 1);
+    }
+
+    int f(int i, int j) {
+        if (i == j) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+
+        dp[i][j] = INT_MAX;
+        for (int k = i; k < j; k++) {
+            // every possible split, and recurse
+            dp[i][j] = min(dp[i][j], f(i, k) + f(k + 1, j) + (g[i][k] * g[k + 1][j]));
+        }
+        return dp[i][j];
+    }
+};
+
+// the below is bottom up, personally, I want to try using top-down more
 class Solution {
   public:
     int mctFromLeafValues(vector<int> &arr) {
---
+++ Main/DynamicProgramming/Review/Interval/MCM.cc
@@ -0,0 +1,67 @@
+#include <climits>
+#include <iostream>
+using namespace std;
+/*
+Input: arr[] = {40, 20, 30, 10, 30}
+Output: 26000
+Explanation:There are 4 matrices of dimensions 40×20, 20×30, 30×10, 10×30.
+Let the input 4 matrices be A, B, C and D.
+The minimum number of  multiplications are obtained by
+putting parenthesis in following way (A(BC))D.
+The minimum is 20*30*10 + 40*20*10 + 40*10*30
+
+Input: arr[] = {1, 2, 3, 4, 3}
+Output: 30
+Explanation: There are 4 matrices of dimensions 1×2, 2×3, 3×4, 4×3.
+Let the input 4 matrices be A, B, C and D.
+The minimum number of multiplications are obtained by
+putting parenthesis in following way ((AB)C)D.
+The minimum number is 1*2*3 + 1*3*4 + 1*4*3 = 30
+*/
+
+// solution is similar to how you would solve minimum cost tree from leaf values
+// recursively, we split the interval in half, and look for the minimum from there
+
+int dp[100][100];
+// top-down memoized recursion
+int fmem(int p[], int i, int j) {
+    // base
+    if (i == j) {
+        return 0;
+    }
+
+    // check if memoized
+    if (dp[i][j] != -1) return dp[i][j];
+
+    dp[i][j] = INT_MAX;
+    // every possible split
+    for (int k = i; k < j; k++) {
+        dp[i][j] = min(dp[i][j], (p[i - 1] * p[k] * p[j]) + fmem(p, i, k) + fmem(p, k + 1, j));
+    }
+    return dp[i][j];
+}
+
+// naive recursive solution
+int fnaive(int p[], int i, int j) {
+    // base
+    if (i == j) {
+        return 0;
+    }
+
+    int minOps = INT_MAX;
+    // every possible split
+    for (int k = i; k < j; k++) {
+        minOps = min(minOps, (p[i - 1] * p[k] * p[j]) + fnaive(p, i, k) + fnaive(p, k + 1, j));
+    }
+    return minOps;
+}
+
+// Driver Code
+int main() {
+    int arr[] = {1, 2, 3, 4, 3};
+    int N = sizeof(arr) / sizeof(arr[0]);
+    // Function call
+    memset(dp, -1, sizeof(dp));
+    cout << "Minimum number of multiplications is " << fmem(arr, 1, N - 1) << endl;
+    return 0;
+}
