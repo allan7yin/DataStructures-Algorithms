c04304ad5eef37051779cb2f6c0284d255281f88
Allan Yin <a7yin@uwaterloo.ca>
2025-08-09 02:06:09 -0700

    daily

---
+++ src/2025/WarRoom/WarGames/231.cc
@@ -0,0 +1,13 @@
+#include "../bits/stdc++.h"
+using namespace std;
+
+/**
+ * Idea is, power of is 10...0 so minus 1 will be 0111.111
+ * So, n & (n-1) will be 0 if n is power of 2
+ */
+class Solution {
+  public:
+    bool isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+};
